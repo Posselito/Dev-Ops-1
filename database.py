0e3df6dc56f721b30025bd261e364b9d69a5768d
Posselito <possel.josh@gmail.com>
2017-07-26 19:01:03 -0500

    Functions in db
    Functions implemented for use in UI

--- database.py
+++ database.py
@@ -5,34 +5,50 @@ db = pymysql.connect("localhost", "username", "password", "testdb")
 cursor = db.cursor()
 
 cursor.execute("SELECT VERSION()")
-data = cursor.fetchone()
+version = cursor.fetchone()
 
-print("Version: %s" %data)
-#add new inventory item
-'''
-id = input("Enter ID number: ")
-name = input("Enter product name: ")
-try:
-    #input inventory data
-    cursor.execute("""INSERT INTO INVENTORY VALUES (%s, %s)""", (id, name))
-    #commit to db
-    db.commit()
-    print("Success!")
-except:
-    #rollback incase any error
-    print("Error")
-    db.rollback()
-'''
-#end
-#search for item
-QName = input("What item are you searching for?: ")
+print("Version: %s" %version)
+def addItem():
 
-try:
-    cursor.execute("""SELECT * FROM INVENTORY WHERE NAME = %s""", (QName))
-    data = cursor.fetchone()
-    if data[1] == QName:
-        print("Item in library")
-except:
-    print("Item not in Library")
-#end
+    id = input("Enter ID number: ")
+    name = input("Enter product name: ")
+    try:
+        #input inventory data
+        cursor.execute("""INSERT INTO INVENTORY VALUES (%s, %s)""", (id, name))
+        #commit to db
+        db.commit()
+        print("Success!")
+    except:
+        #rollback incase any error
+        print("Error")
+        db.rollback()
+
+def search():
+    QName = input("What item are you searching for?: ")
+
+    try:
+        cursor.execute("""SELECT * FROM INVENTORY WHERE NAME = %s""", (QName))
+        result = cursor.fetchone()
+        if result[1] == QName:
+            print("Item in library")
+    except:
+        print("Item not in Library")
+
+def displayAll():
+    cursor.execute("""SELECT * FROM INVENTORY""")
+    data = cursor.fetchall()
+    for each in data:
+        print(each[1])
+
+def remove():
+    DName = input("What item do you want to remove?: ")
+    try:
+        cursor.execute("""DELETE FROM INVENTORY WHERE NAME = %s""", (DName))
+        db.commit()
+        print("Item removed")
+    except:
+        ("Item not in library")
+
+#cleanup
+cursor.close()
 db.close()
